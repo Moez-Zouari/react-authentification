6c7d766cbe56e1e815a0440869ca6d360cf1954f
Moez <moez_zouari@hotmail.fr>
2021-06-25 16:05:30 +0100

    Redirecting The User

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,9 +1,11 @@
 import { useRef, useContext } from "react";
+import { useHistory } from "react-router";
 import AuthContext from "../../store/auth-context";
 
 import classes from "./ProfileForm.module.css";
 
 const ProfileForm = () => {
+  const history = useHistory();
   const newPasswordInputRef = useRef();
   const authCtx = useContext(AuthContext);
 
@@ -28,6 +30,7 @@ const ProfileForm = () => {
         },
       }
     ).then((res) => {
+      history.replace("/");
       // Assumption : Always Secceeds
     });
   };
